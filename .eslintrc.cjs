b8bd8e16b650072a8e5cc589965d145702e9e80f
seongjin <seongjin2427@gmail.com>
2023-12-04 20:46:06 +0900

    [chore] set up eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,27 @@ module.exports = {
   root: true,
   env: { browser: true, es2021: true },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended', // 추가
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['react-refresh', 'prettier'], // 추가
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
   },
-}
+};
