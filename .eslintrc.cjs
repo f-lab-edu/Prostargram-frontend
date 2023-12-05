bc59424cfffa20559014bb2674ab1f635a9143a7
seongjin <seongjin2427@gmail.com>
2023-12-05 19:08:30 +0900

    [chore] set up path alias

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,15 +7,31 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:prettier/recommended', // 추가
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
   },
-  plugins: ['react-refresh', 'prettier'], // 추가
+  plugins: ['react-refresh', 'prettier'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        directory: './src',
+      },
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', 'tsx'],
+    },
+  },
   rules: {
+    'import/no-unresolved': 'off',
+    'import/no-absolute-path': 'off',
+    'react/function-component-definition': [
+      2,
+      { namedComponents: ['arrow-function'] },
+    ],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '../public/vite.svg';
+import reactLogo from '@assets/react.svg';
+import viteLogo from '/vite.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
 
   return (
@@ -33,6 +33,6 @@ function App() {
       </p>
     </>
   );
-}
+};
 
 export default App;
