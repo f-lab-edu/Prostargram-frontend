56b1b76f61fd5e2482f58333907359d50872c9c6
seongjin <seongjin2427@gmail.com>
2023-12-12 19:33:10 +0900

    [chore] install packages and set up plugin
    - react-router dom, @types/react-router-dom
    - @vanilla-extract/css, @vanilla-extract/vite-plugin, @vanilla-extract/recipes
    - vite.config.ts -> added vanilla-extract plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    vanillaExtractPlugin({
+      identifiers: 'debug',
+    }),
+  ],
 });
