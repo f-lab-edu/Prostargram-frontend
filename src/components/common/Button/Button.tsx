224a0dcdc16de13804a14066453596a235a5e78c
seongjin <seongjin2427@gmail.com>
2023-12-18 21:12:29 +0900

    [feat] added common Button Component - variants   - fill: ['blue', 'gray', 'red']   - size: ['small', 'medium', 'large']   - disabled: ['true']

---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,31 @@
+import { ButtonHTMLAttributes, ReactNode } from 'react';
+import * as Styles from './Button.css';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  size?: 'small' | 'medium' | 'large';
+  fill?: 'blue' | 'red' | 'gray';
+}
+
+const Button = ({ children, fill, size, disabled, ...props }: ButtonProps) => {
+  return (
+    <button
+      className={Styles.buttonStyle({
+        fill,
+        size,
+        disabled,
+      })}
+      disabled={disabled}
+      {...props}
+    >
+      {disabled ? '로딩' : children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  fill: 'blue',
+  size: 'small',
+};
+
+export default Button;
