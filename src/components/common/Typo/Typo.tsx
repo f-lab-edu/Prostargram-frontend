3b44d7f4464f7d80f3fcf142da5a58206673a89d
seongjin <seongjin2427@gmail.com>
2023-12-29 20:09:30 +0900

    [feat] added Typo common Component - variants   - fontSize: [...theme fontSize]   - color: [...theme colors]   - textDecoreation: ['underline']

---
+++ src/components/common/Typo/Typo.tsx
@@ -0,0 +1,59 @@
+import { CSSProperties, ReactNode } from 'react';
+import { vars } from '@styles/theme.css';
+import * as Styles from './Typo.css';
+
+interface TypoProps {
+  children?: ReactNode;
+  as: 'span' | 'div' | 'p' | 'h1' | 'h2' | 'h3';
+  fontSize?: keyof typeof vars.font;
+  color?: keyof typeof vars.colors;
+  underline?: boolean;
+  textAlign?: CSSProperties['textAlign'];
+  margin?: CSSProperties['margin'];
+  marginTop?: CSSProperties['marginTop'];
+  marginBottom?: CSSProperties['marginBottom'];
+  marginLeft?: CSSProperties['marginLeft'];
+  marginRight?: CSSProperties['marginRight'];
+}
+
+const Typo = ({
+  children,
+  as: Component = 'span',
+  fontSize,
+  color,
+  textAlign,
+  underline,
+  margin,
+  marginTop,
+  marginBottom,
+  marginLeft,
+  marginRight,
+}: TypoProps) => {
+  return (
+    <Component
+      style={{
+        textAlign,
+        margin,
+        marginTop,
+        marginBottom,
+        marginLeft,
+        marginRight,
+      }}
+      className={Styles.typography({
+        fontSize,
+        color,
+        textDecoration: underline ? 'underline' : undefined,
+      })}
+    >
+      {children}
+    </Component>
+  );
+};
+
+Typo.defaultProps = {
+  as: 'span',
+  fontSize: 'body-16',
+  color: 'gray-1',
+};
+
+export default Typo;
