e926189721a1525e7849193ecc8a3a99fa2fc47b
seongjin <seongjin2427@gmail.com>
2024-02-16 21:59:25 +0900

    [fix] change style, coupled className props with component style className
    - update responsive height

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,30 +1,42 @@
 import { ButtonHTMLAttributes } from 'react';
+
 import * as Styles from './Button.css';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   size?: 'small' | 'medium' | 'large';
   fill?: 'blue' | 'red' | 'gray';
+  className?: string;
 }
 
-const Button = ({ children, fill, size, disabled, ...props }: ButtonProps) => {
+const Button = ({
+  children,
+  fill,
+  size,
+  disabled,
+  className,
+  ...props
+}: ButtonProps) => {
   return (
     <button
-      className={Styles.buttonStyle({
-        fill,
-        size,
-        disabled,
-      })}
+      className={[
+        Styles.buttonStyle({
+          fill,
+          size,
+          disabled,
+        }),
+        className,
+      ].join(' ')}
       disabled={disabled}
       {...props}
     >
-      {disabled ? '로딩' : children}
+      {children}
     </button>
   );
 };
 
 Button.defaultProps = {
   fill: 'blue',
-  size: 'small',
+  size: 'large',
 };
 
 export default Button;
