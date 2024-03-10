be252e017312aae7b774dfd7f25522d5aa2227e1
seongjin <seongjin2427@gmail.com>
2024-03-10 13:13:14 +0900

    [feat] update Logo Component for applying GitHub Icon

--- src/components/common/Logo/Logo.css.ts
+++ src/components/common/Logo/Logo.css.ts
@@ -2,18 +2,42 @@ import { responsiveStyle } from '@styles/styles.css';
 import { vars } from '@styles/theme.css';
 import { style } from '@vanilla-extract/css';
 
-export const LogoStyle = style([
+export const link = style({
+  display: 'block',
+  textAlign: 'center',
+});
+
+export const logo = style([
   {
+    position: 'relative',
     fontSize: vars.font['bold-32'].fontSize,
     fontWeight: vars.font['bold-32'].fontWeight,
     color: vars.colors['primary-1'],
     textShadow: '2px 2px 5px rgba(0, 0, 0, 0.25)',
-    textAlign: 'center',
     cursor: 'pointer',
   },
   responsiveStyle({
     desktop: {
       fontSize: 60,
+    },
+  }),
+]);
+
+export const github = style([
+  {
+    position: 'absolute',
+    width: '1.875rem',
+    height: '1.875rem',
+    right: '-20px',
+    top: '-10px',
+    transform: 'rotate(30deg)',
+  },
+  responsiveStyle({
+    desktop: {
+      width: '4.375rem',
+      height: '4.375rem',
+      right: '-40px',
+      top: '-30px',
     },
   }),
 ]);
