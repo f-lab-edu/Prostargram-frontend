75230ea5e7b5b88d9d68477703d8c791de4b8652
seongjin <seongjin2427@gmail.com>
2024-05-31 16:51:07 +0900

    [feat] update MyLink Component using EditMyLink, ReadMyLink Component

--- src/app/my/components/MyLink/MyLink.tsx
+++ src/app/my/components/MyLink/MyLink.tsx
@@ -1,120 +1,44 @@
 'use client';
 
-import Link from 'next/link';
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 
-import Button from '@/components/common/Button';
-import Input from '@/components/common/Input';
+import { updateMyLinks } from '@/api/my';
+import EditMyLinks from './EditMyLinks';
+import ReadMyLinks from './ReadMyLinks';
 
-import { flexCenter } from '@/styles/common.css';
 import * as Styles from './MyLink.css';
-
-const ICON_LIST = ['github', 'naver'];
-
-interface SocialAccountProps {
-  link: string;
-  isEdit: boolean;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-}
-
-const SocialAccount = ({ link, isEdit, onChange }: SocialAccountProps) => {
-  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    onChange(e);
-  };
-
-  return (
-    <li key={link} className={Styles.myLinkInputWrapper}>
-      <i className={flexCenter}>
-        {ICON_LIST.find((target) => link.includes(target)) || 'default'}
-      </i>
-      {isEdit ? (
-        <>
-          <Input
-            variants="noneBorder"
-            size="small"
-            className={Styles.myLinkInput}
-            onChange={changeHandler}
-            defaultValue={link}
-          />
-          <Button
-            type="button"
-            fill="red"
-            className={`${flexCenter} ${Styles.remoteButton}`}
-          >
-            X
-          </Button>
-        </>
-      ) : (
-        <Link className={Styles.myLink} href={link}>
-          <p>{link}</p>
-        </Link>
-      )}
-    </li>
-  );
-};
 
 interface MyLinkProps {
   links: string[];
 }
 
 const MyLink = ({ links }: MyLinkProps) => {
-  const [myLinks, setMyLinks] = useState<string[]>([...links]);
+  const [myLinks, setMyLinks] = useState(links);
   const [isEdit, setIsEdit] = useState<boolean>(false);
-  const isMine = true;
 
   const toggleEdit = () => setIsEdit((prev) => !prev);
 
-  const saveLinks = (e: FormEvent<HTMLFormElement>) => {
-    console.log('execute saveLinks function');
+  const saveLinks = async (e: ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setIsEdit(false);
-  };
 
-  const changeHandler = (e: ChangeEvent<HTMLInputElement>, index: number) => {
-    const next = [...myLinks];
-    next[index] = e.target.value;
-    setMyLinks(next);
+    const formData = new FormData(e.currentTarget);
+    const submittedValue = [...formData.values()].filter((v) => Boolean(v));
+
+    if (submittedValue.length) {
+      const result = await updateMyLinks(submittedValue);
+      setMyLinks(result);
+    }
+
+    toggleEdit();
   };
 
   return (
-    <form className={Styles.myLinkContainer} onSubmit={saveLinks}>
-      <ul className={Styles.iconWithMyLinkWrapper}>
-        {myLinks.map((link, index) => (
-          <SocialAccount
-            key="link"
-            link={link}
-            isEdit={isEdit}
-            onChange={(e) => changeHandler(e, index)}
-          />
-        ))}
-
-        {myLinks.length < 3 && (
-          <Button type="button" fill="white" size="small">
-            +
-          </Button>
-        )}
-      </ul>
-
-      <div className={Styles.editButtonWrapper}>
-        {isMine && isEdit ? (
-          <>
-            <Button type="submit" className={Styles.editButton}>
-              저장
-            </Button>
-            <Button
-              type="button"
-              className={Styles.editButton}
-              onClick={toggleEdit}
-            >
-              취소
-            </Button>
-          </>
-        ) : (
-          <Button className={Styles.editButton} onClick={toggleEdit}>
-            수정
-          </Button>
-        )}
-      </div>
+    <form onSubmit={saveLinks} className={Styles.myLinkContainer}>
+      {isEdit ? (
+        <EditMyLinks links={myLinks} onToggle={toggleEdit} />
+      ) : (
+        <ReadMyLinks links={myLinks} onToggle={toggleEdit} />
+      )}
     </form>
   );
 };
