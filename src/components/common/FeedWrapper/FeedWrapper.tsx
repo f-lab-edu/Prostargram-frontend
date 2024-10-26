6f60db5d6bb291e7c50836431b879c4e8e455452
seongjin <seongjin2427@gmail.com>
2024-10-26 22:26:39 +0900

    [feat] added FeedWrapper Component - FeedWrapper depends on whether there is pathname value "p" or not - If pathname "p" value of browser navigation is existed, FeedWrapper render children props

---
+++ src/components/common/FeedWrapper/FeedWrapper.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+import Modal from '../Modal';
+
+interface FeedWrapperProps {
+  children?: ReactNode;
+  modalWidth?: string;
+}
+
+const FeedWrapper = ({ modalWidth = '1070px', children }: FeedWrapperProps) => {
+  const router = useRouter();
+  const searchParams = new URLSearchParams(useSearchParams()).get('p');
+
+  const closeFeed = (bool: boolean) => {
+    console.log(bool);
+    router.push('/');
+  };
+
+  if (!searchParams) {
+    return null;
+  }
+
+  return (
+    <Modal width={modalWidth} onClose={closeFeed}>
+      {children}
+    </Modal>
+  );
+};
+
+export default FeedWrapper;
