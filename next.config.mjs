b71722910de19bbc030c245a968ed267df13fe74
lee1nna <dlgkssk0209@gmail.com>
2024-12-02 23:38:23 +0900

    [feat]: link detail common feed API

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'encrypted-tbn0.gstatic.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'png.pngtree.com',
+      },
     ],
   },
   webpack(config) {
--- src/api/feed/apis.ts
+++ src/api/feed/apis.ts
@@ -12,6 +12,18 @@ const getFeeds = (page: number) => {
   return res;
 };
 
+const getDetailCommonFeed = (postId: string) => {
+  const res = authInstance<Feed.FeedData>({
+    method: 'get',
+    url: `/posts/${postId}/basic-post`,
+    headers: {
+      Authorization:
+        'Bearer eyJhbGciOiJIUzUxMiJ9.eyJhdXRob3JpdHkiOiJOT1JNQUxfVVNFUiIsInRva2VuVHlwZSI6IkFDQ0VTU19UT0tFTiIsInN1YiI6IjEiLCJleHAiOjE3NDI3MTUzMDV9.eFVccnbsYRqpJFzg2rL5LNgsMLoEFPESoj3W4e5bgm2Uzy7-DCjA6hgixDZ8MYgXeV6q7VetKQTab-pH-g0caw',
+    },
+  });
+  return res;
+};
+
 const createCommonFeed = (data: Feed.BasicPostRequestBody) => {
   const res = authInstance({
     method: 'post',
@@ -25,4 +37,4 @@ const createCommonFeed = (data: Feed.BasicPostRequestBody) => {
   return res;
 };
 
-export { getFeeds, createCommonFeed };
+export { getFeeds, getDetailCommonFeed, createCommonFeed };
