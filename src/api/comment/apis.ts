1edb12cb61f3c0e648abe015c05da47b16c7d8c2
lee1nna <dlgkssk0209@gmail.com>
2024-12-11 22:27:54 +0900

    [feat]: set content type 'text/plain' to write comment API

--- src/api/comment/apis.ts
+++ src/api/comment/apis.ts
@@ -44,6 +44,7 @@ const writeComment = (postId: number, data: string) => {
     headers: {
       Authorization:
         'Bearer eyJhbGciOiJIUzUxMiJ9.eyJhdXRob3JpdHkiOiJOT1JNQUxfVVNFUiIsInRva2VuVHlwZSI6IkFDQ0VTU19UT0tFTiIsInN1YiI6IjEiLCJleHAiOjE3NDI3MTUzMDV9.eFVccnbsYRqpJFzg2rL5LNgsMLoEFPESoj3W4e5bgm2Uzy7-DCjA6hgixDZ8MYgXeV6q7VetKQTab-pH-g0caw',
+      'Content-Type': 'text/plain',
     },
   });
   return res;
