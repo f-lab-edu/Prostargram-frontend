51e6c315addc6a4cfcd4b073780060e23743f29b
seongjin <seongjin2427@gmail.com>
2025-01-06 15:46:48 +0900

    [fix] add Suspense to resolve missing Suspense Boundary error when using useSearchParams

--- src/app/(MainLayout)/page.tsx
+++ src/app/(MainLayout)/page.tsx
@@ -1,15 +1,19 @@
-import LocalQueryErrorBoundary from '@/components/common/LocalQueryErrorBoundary';
+import { Suspense } from 'react';
 
+import LocalQueryErrorBoundary from '@/components/common/LocalQueryErrorBoundary';
 import FollowingList from './components/FollowingList/FollowingList';
 import FeedList from './components/FeedList/FeedList';
+
 import styles from './page.module.scss';
 
 const MainPage = () => {
   return (
     <div className={styles.container}>
       <LocalQueryErrorBoundary>
-        <FollowingList userId={1} />
-        <FeedList />
+        <Suspense>
+          <FollowingList userId={1} />
+          <FeedList />
+        </Suspense>
       </LocalQueryErrorBoundary>
     </div>
   );
