7eb259decc95b4cf7e97fee479a8497b83429d99
이토 <59536977+seongjin2427@users.noreply.github.com>
2025-01-08 21:56:03 +0900

    [feat] 마이페이지 서버 요청 로직 업데이트 (#43)
    * [feat] update logic of request profile information
    
    * [feat] update profile and MyInformation logic

--- src/app/(MainLayout)/components/FeedTextContent/FeedTextContent.tsx
+++ src/app/(MainLayout)/components/FeedTextContent/FeedTextContent.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 import { compactTimeFormatter } from '@/utils/formatter';
-import ProfileFollowButton from '@/app/my/components/Profile/ProfileFollowButton';
+import ProfileFollowButton from '@/app/profile/components/Profile/ProfileFollowButton';
 
 import styles from './FeedTextContent.module.scss';
 
