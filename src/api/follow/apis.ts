ff6501e470ba73f6d440691976fee01ac5de499e
lee1nna <dlgkssk0209@gmail.com>
2025-01-10 18:37:20 +0900

    [style]: modify skeleton feed UI

--- src/api/follow/apis.ts
+++ src/api/follow/apis.ts
@@ -1,4 +1,4 @@
-import { authInstance, defaultInstance } from '../httpRequest';
+import { authInstance } from '../httpRequest';
 
 const followUser = (data: { fromUserId: number; toUserId: number }) => {
   const res = authInstance({
@@ -26,13 +26,9 @@ interface FollowingRes {
 }
 
 const getFollowingList = (userId: number) => {
-  const res = defaultInstance<FollowingRes[]>({
+  const res = authInstance<FollowingRes[]>({
     method: 'get',
     url: `/users/${userId}/followings`,
-    headers: {
-      Authorization:
-        'Bearer eyJhbGciOiJIUzUxMiJ9.eyJhdXRob3JpdHkiOiJOT1JNQUxfVVNFUiIsInRva2VuVHlwZSI6IkFDQ0VTU19UT0tFTiIsInN1YiI6IjEiLCJleHAiOjE3NDI3MTUzMDV9.eFVccnbsYRqpJFzg2rL5LNgsMLoEFPESoj3W4e5bgm2Uzy7-DCjA6hgixDZ8MYgXeV6q7VetKQTab-pH-g0caw',
-    },
   });
   return res;
 };
--- src/components/common/SkletonFeed/SkeletonFeed.tsx
+++ src/components/common/SkletonFeed/SkeletonFeed.tsx
@@ -1,8 +1,20 @@
 import styles from './SkeletonFeed.module.scss';
 
 const SkeletonFeed = () => {
-  // TODO: feed에 맞게 style 변경
-  return <div className={styles.container} />;
+  return (
+    <div className={styles.container}>
+      <header className={styles.top}>
+        <div className={styles.profile_img} />
+        <div className={styles.name} />
+      </header>
+      <main className={styles.center} />
+      <footer className={styles.bottom}>
+        <div className={styles.bottom_top} />
+        <div className={styles.bottom_center} />
+        <div className={styles.bottom_bottom} />
+      </footer>
+    </div>
+  );
 };
 
 export default SkeletonFeed;
